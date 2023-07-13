e4cd089f1f1e62d06e740a7f4db686c8b0a5d818
Luke Bakken <luke@bakken.io>
2023-07-13 14:24:51 -0700

    Initial commit, uses OTP 25

---
+++ run-node.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+declare -r nodename="${1:-a}"
+
+declare -r script_dir="$(realpath "$(dirname "$BASH_SOURCE")")"
+
+declare -r erl_ssl_path="$(erl -noinput -eval 'io:format("~s~n", [filename:dirname(code:which(inet_tls_dist))])' -s init stop)"
+
+if [[ $nodename == 'a' ]]
+then
+    erl -pa "$erl_ssl_path" -proto_dist inet_tls -ssl_dist_optfile "$script_dir/inet-dist-tls.config" -sname a
+else
+    erl -pa "$erl_ssl_path" -proto_dist inet_tls -ssl_dist_optfile "$script_dir/inet-dist-tls.config" -sname b -eval "net_kernel:connect_node(a@$HOSTNAME), erlang:display(nodes())"
+fi
